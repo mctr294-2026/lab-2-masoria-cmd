75a6fcd76c4dd8c03f48bc8a83ab0d5508d48790
Nicolaus <masoria@ualberta.ca>
2026-01-27 01:55:20 -0700

    Attempted the bisection method. The process involves FIRST checking if IVT applies. If not, use the next method! I have not implemented that yet. Then, generally, given that there is some point between [a,b] that IS zero, We find the exact middle point between the two. "Bisecting" them. Here, we call that point c = (a+b) / 2. Then, we apply IVT again. Given that f(a) * f(c) < 0, we know that within [a,c] there is a zero Likewise, f(a) * f(c) > 0, we know that within [a,c] there is no zero, but [c,b] does. So we equate a or b to c whichever case there is. Then, we keep evaluating c over and over until we get the desired precision. At least, that's what I think?

--- src/roots/main.cpp
+++ src/roots/main.cpp
@@ -25,9 +25,40 @@ double poly2_deriv(double x)
     return 6 * x * x - 8 * x + 3;
 }
 
-int main(int argc, char **argv)
-{
-    double root = 0.0;
+double epsilon{0.01};
+
+double bisectMethod(double a, double b) {
+    // Initialize interval [a, b] and the calculated c value
+
+    double c{};
+            
+        // While the distance between b and a are not small enough, run this loop
+        while (std::abs(b - a) >= 1e-7) {
+
+                // Calculate the mid points 
+                c = (a + b) / 2.00;
+
+                std::cout << '\n' << c << '\n';
+                // If f(c) is really close to 0, show that the root is at c
+                if (std::abs(poly1(c)) < 1e-12) {
+                    break;
+                }
+
+                // If f(c) is not really close to 0, we can iterate again by seeing if the root lies between a and c or c and b
+                if (poly1(c) * poly1(a) < 0) {
+                    b =  c; }
+                    else {
+                        a = c;
+                }
+        
+        }
+    double root = c;
+    std::cout << "Bisection: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;   
+    
+    return root;
+
+}
+int main(int argc, char **argv) {       
     const char *method_names[] = {"Bisection", "Regula Falsi", "Newton-Raphson", "Secant"};
 
     std::cout << std::fixed << std::setprecision(10);
@@ -37,82 +68,80 @@ int main(int argc, char **argv)
     // Test poly1 with all methods
     std::cout << "=== poly1(x) = x^3 - x^2 + 2, bracket [-200, 300] ===" << std::endl;
 
-    if (bisection(poly1, -200.0, 300.0, &root))
-    {
-        std::cout << "Bisection: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Bisection: failed" << std::endl;
-    }
-
-    if (regula_falsi(poly1, -200.0, 300.0, &root))
-    {
-        std::cout << "Regula Falsi: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Regula Falsi: failed" << std::endl;
-    }
-
-    if (newton_raphson(poly1, poly1_deriv, -200.0, 300.0, -1.0, &root))
-    {
-        std::cout << "Newton-Raphson: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Newton-Raphson: failed" << std::endl;
-    }
-
-    if (secant(poly1, -200.0, 300.0, -1.0, &root))
-    {
-        std::cout << "Secant: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Secant: failed" << std::endl;
-    }
-
-    std::cout << std::endl;
-
-    // Test poly2 with all methods
-    std::cout << "=== poly2(x) = 2x^3 - 4x^2 + 3x, bracket [-1, 1] ===" << std::endl;
-
-    if (bisection(poly2, -1.0, 1.0, &root))
-    {
-        std::cout << "Bisection: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Bisection: failed" << std::endl;
-    }
-
-    if (regula_falsi(poly2, -1.0, 1.0, &root))
-    {
-        std::cout << "Regula Falsi: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Regula Falsi: failed" << std::endl;
-    }
-
-    if (newton_raphson(poly2, poly2_deriv, -1.0, 1.0, -0.5, &root))
-    {
-        std::cout << "Newton-Raphson: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Newton-Raphson: failed" << std::endl;
-    }
-
-    if (secant(poly2, -1.0, 1.0, -0.5, &root))
-    {
-        std::cout << "Secant: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
-    }
-    else
-    {
-        std::cout << "Secant: failed" << std::endl;
-    }
+    bisectMethod(-200.0, 300.0);
 
     return 0;
 }
+
+    
+
+//     if (regula_falsi(poly1, -200.0, 300.0, &root))
+//     {
+//         std::cout << "Regula Falsi: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Regula Falsi: failed" << std::endl;
+//     }
+
+//     if (newton_raphson(poly1, poly1_deriv, -200.0, 300.0, -1.0, &root))
+//     {
+//         std::cout << "Newton-Raphson: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Newton-Raphson: failed" << std::endl;
+//     }
+
+//     if (secant(poly1, -200.0, 300.0, -1.0, &root))
+//     {
+//         std::cout << "Secant: root = " << root << ", poly1(root) = " << poly1(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Secant: failed" << std::endl;
+//     }
+
+//     std::cout << std::endl;
+
+//     // Test poly2 with all methods
+//     std::cout << "=== poly2(x) = 2x^3 - 4x^2 + 3x, bracket [-1, 1] ===" << std::endl;
+
+//     if (bisection(poly2, -1.0, 1.0, &root))
+//     {
+//         std::cout << "Bisection: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Bisection: failed" << std::endl;
+//     }
+
+//     if (regula_falsi(poly2, -1.0, 1.0, &root))
+//     {
+//         std::cout << "Regula Falsi: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Regula Falsi: failed" << std::endl;
+//     }
+
+//     if (newton_raphson(poly2, poly2_deriv, -1.0, 1.0, -0.5, &root))
+//     {
+//         std::cout << "Newton-Raphson: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Newton-Raphson: failed" << std::endl;
+//     }
+
+//     if (secant(poly2, -1.0, 1.0, -0.5, &root))
+//     {
+//         std::cout << "Secant: root = " << root << ", poly2(root) = " << poly2(root) << std::endl;
+//     }
+//     else
+//     {
+//         std::cout << "Secant: failed" << std::endl;
+//     }
+
+//     return 0;
+// }
