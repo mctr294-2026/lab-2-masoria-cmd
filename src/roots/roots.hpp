6b7c9c6a1f7789ba2f5a013827ace508ecedcc79
Nicolaus <masoria@ualberta.ca>
2026-01-28 08:46:50 -0700

    I realized we weren't supposed to change the .hpp file. Those are our headers that point to our function in .cpp That allows the tests to be compiled properly. So, I restructured my logic and put it in a created roots.cpp and brought back roots.hpp and CMakelist.txt for proper test building Bisection failed from returning true after seeing there is no root, so I caught that error by adding an if statement

--- src/roots/roots.hpp
+++ src/roots/roots.hpp
@@ -8,39 +8,7 @@
  */
 bool bisection(std::function<double(double)> f,
                double a, double b,
-               double *root)
-
-               {
-    // Initialize interval [a, b] and the calculated c value
-    int counter{};
-    double c{0};
-            
-        // While the distance between b and a are not small enough, run this loop
-                while (std::abs(b - a) >= 1e-6) {
-
-                // Calculate the mid points 
-                c = (a + b) / 2.00;
-                // std::cout << c << std::endl;
-                counter = counter + 1;
-                // If f(c) is really close to 0, show that the root is at c
-                if (std::abs(f(c)) < 1e-6) {
-                    break;
-                }
-
-                // If f(c) is not really close to 0, we can iterate again by seeing if the root lies between a and c or c and b
-                if (f(c) * f(a) < 0) {
-                    b =  c; }
-                    else {
-                        a = c;
-                }
-        
-        }
-    std::cout << "iterations = " << counter << ", ";
-    *root = c;  
-    
-    return true, counter;
-}
-    
+               double *root);
 
 /* Tries to find a zero crossing in f() in the interval [a,b] with the
  * false positive / regula falsi method
@@ -51,42 +19,7 @@ bool bisection(std::function<double(double)> f,
  */
 bool regula_falsi(std::function<double(double)> f,
                   double a, double b,
-                  double *root)
-
-                  {
-    // Initialize interval [a, b] and the calculated c value
-
-    double c{a};
-    double cd{};
-    int counter{0};
-            
-        // Keep running the loop until a set
-                while (std::abs(f(c)) >  1e-6) {
-                // Calculate the x intersections 
-                counter = counter + 1;
-                c = a - ((f(a)*(b - a)) / (f(b)- f(a)));
-                // std::cout << c << std::endl;
-
-                if (std::abs(f(c - cd)) < 1e-15) {
-                    break;
-                }
-
-                // If f(c) is not really close to 0, we can iterate again by seeing if the root lies between a and c or c and b
-                if (f(c) * f(a) < 0) {
-                    b =  c; }
-                    else {
-                        a = c;
-                }
-        
-        }
-    std::cout << "iterations = " << counter << ", ";
-    *root = c;
-    
-    return true, counter;
-
-
-}
-
+                  double *root);
 
 /* Tries to find a zero crossing in f() in the interval [a,b] with
  * the netwon-raphson method, given a function that computes the
@@ -98,25 +31,7 @@ bool regula_falsi(std::function<double(double)> f,
 bool newton_raphson(std::function<double(double)> f,
                     std::function<double(double)> g,
                     double a, double b, double c,
-                    double *root)
-                    {
-    // Initialize interval [a, b] and the starting guess c value
-    int counter{0};
-        // Infinite while loop for now
-                while (std::abs(f(c)) > 1e-6) {
-
-                // Calculate the root 
-                c = c - (f(c)/g(c));
-                // std::cout << c << std::endl;
-                counter = counter + 1;
-                
-        }
-    std::cout << "iterations = " << counter << ", ";
-    *root = c;
-    
-    return true, counter;
-
-}
+                    double *root);
 
 /* Tries to find a zero crossing in f() in the interval [a,b] with
  * the secant method, given a starting guess c.
@@ -126,24 +41,4 @@ bool newton_raphson(std::function<double(double)> f,
  */
 bool secant(std::function<double(double)> f,
             double a, double b, double c,
-            double *root)
-            {
-    // Initialize interval [a, b] and the calculated c value
-     // Initialize interval [a, b] and the starting guess c value
-
-    int counter{0};
-        // Infinite while loop for now
-                while (std::abs(f(c)) > 1e-6) {
-
-                // Calculate the root 
-                c = a - ((b - a) / (f(b) - f(a))) * f(a);
-                // std::cout << c << std::endl;
-                counter = counter + 1;
-                a = b;
-                b = c;
-        }
-    *root = c;
-    std::cout << "iterations = " << counter << ", ";
-    return true, counter;
-
-}
+            double *root);
